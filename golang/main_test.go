fe8eb33ed20b9ba986039edcb53524e1cc3a4675
Adol Rodriguez <adolsalamanca@gmail.com>
2022-05-15 11:02:08 +0200

    completely convert tests to table driven, delete not needed gitignore

--- golang/main_test.go
+++ golang/main_test.go
@@ -7,7 +7,6 @@ import (
 	"log"
 	"sync"
 	"testing"
-	"time"
 )
 
 type scenarioTester struct {
@@ -110,219 +109,88 @@ func TestTaskList_executeWithErrors(t *testing.T) {
 	}
 }
 
-func TestRunToday(t *testing.T) {
-	runParams := NewTaskListRunParams()
-
-	log.SetOutput(io.Discard)
-	defer runParams.inPR.Close()
-	defer runParams.outPR.Close()
-	tester := &scenarioTester{
-		T:          t,
-		inWriter:   runParams.inPW,
-		outReader:  runParams.outPR,
-		outScanner: bufio.NewScanner(runParams.outPR),
+func TestTaskList_executeWithReadLines(t *testing.T) {
+	type args struct {
+		cmdCommands []string
 	}
-
-	initTaskListAndRun(runParams.wg, runParams.inPR, runParams.outPW, runParams.errorsChan, runParams.shutdownChan)
-
-	// run command-line scenario
-	log.Println("(show empty)")
-	tester.execute("show")
-
-	log.Println("(add project)")
-	tester.execute("add project secrets")
-	log.Println("(add tasks)")
-	tester.execute("add task secrets Eat more donuts.")
-	tester.execute("add task secrets Destroy all humans.")
-
-	log.Println("(deadline inclusion)")
-	tester.execute("deadline 1 20200721")
-	tester.execute("deadline 2 20200730")
-
-	log.Println("(today)")
-	tester.execute("today")
-	tester.readLines([]string{
-		"secrets",
-		"    [ ] 1: (20200721) Eat more donuts.",
-		"    [ ] 2: (20200730) Destroy all humans.",
-		"",
-	})
-
-	time.Now()
-
-	log.Println("(quit)")
-	tester.execute("quit")
-
-	// make sure main program has quit
-	runParams.inPW.Close()
-	runParams.wg.Wait()
-
-	var err error
-	select {
-	case err = <-runParams.errorsChan:
-		log.Printf("program failed, %s", err)
-	case <-runParams.shutdownChan:
-		log.Println("finished")
+	tests := []struct {
+		name      string
+		args      args
+		readLines []string
+	}{
+		{
+			name: "after executing run, check and show commands, list of both checked and pending tasks is returned",
+			args: args{
+				cmdCommands: []string{"add project secrets", "add task secrets Eat more donuts.", "add task secrets Destroy all humans.", "add project training", "add task training Four Elements of Simple Design", "add task training SOLID", "add task training Coupling and Cohesion", "add task training Primitive Obsession", "add task training Outside-In TDD", "add task training Interaction-Driven Design", "check 1", "check 3", "check 5", "check 6", "show"},
+			},
+			readLines: []string{
+				"secrets",
+				"    [X] 1: Eat more donuts.",
+				"    [ ] 2: Destroy all humans.",
+				"",
+				"training",
+				"    [X] 3: Four Elements of Simple Design",
+				"    [ ] 4: SOLID",
+				"    [X] 5: Coupling and Cohesion",
+				"    [X] 6: Primitive Obsession",
+				"    [ ] 7: Outside-In TDD",
+				"    [ ] 8: Interaction-Driven Design",
+				"",
+			},
+		},
+		{
+			name: "after executing run, deadline and show commands, list of pending tasks with deadlines is returned",
+			args: args{
+				cmdCommands: []string{"add project secrets", "add task secrets Eat more donuts.", "add task secrets Destroy all humans.", "deadline 1 20200721", "deadline 2 20200730", "show"},
+			},
+			readLines: []string{
+				"secrets",
+				"    [ ] 1: (20200721) Eat more donuts.",
+				"    [ ] 2: (20200730) Destroy all humans.",
+				"",
+			},
+		},
 	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			runParams := NewTaskListRunParams()
+			defer runParams.inPR.Close()
+			defer runParams.outPR.Close()
+			defer close(runParams.shutdownChan)
+			defer close(runParams.errorsChan)
 
-	if err != nil {
-		t.Fail()
+			tester := &scenarioTester{
+				T:          t,
+				inWriter:   runParams.inPW,
+				outReader:  runParams.outPR,
+				outScanner: bufio.NewScanner(runParams.outPR),
+			}
+			initTaskListAndRun(runParams.wg, runParams.inPR, runParams.outPW, runParams.errorsChan, runParams.shutdownChan)
+
+			for _, command := range tt.args.cmdCommands {
+				log.Println(command)
+				tester.execute(command)
+			}
+
+			tester.readLines(tt.readLines)
+			tester.execute("quit")
+
+			runParams.inPW.Close()
+			runParams.wg.Wait()
+
+			var err error
+			select {
+			case err = <-runParams.errorsChan:
+				log.Printf("program failed, %s", err)
+			case <-runParams.shutdownChan:
+				log.Println("finished")
+			}
+
+			if err != nil {
+				t.Fail()
+			}
+		})
 	}
-}
-
-func TestRunWithDeadline(t *testing.T) {
-	// setup input/output
-	log.SetOutput(io.Discard)
-	inPR, inPW := io.Pipe()
-	defer inPR.Close()
-	outPR, outPW := io.Pipe()
-	defer outPR.Close()
-	tester := &scenarioTester{
-		T:          t,
-		inWriter:   inPW,
-		outReader:  outPR,
-		outScanner: bufio.NewScanner(outPR),
-	}
-
-	// run main program
-	var wg sync.WaitGroup
-	shutdownChan := make(chan bool)
-	errorsChan := make(chan error)
-	initTaskListAndRun(wg, inPR, outPW, errorsChan, shutdownChan)
-	// run command-line scenario
-	log.Println("(show empty)")
-	tester.execute("show")
-
-	log.Println("(add project)")
-	tester.execute("add project secrets")
-	log.Println("(add tasks)")
-	tester.execute("add task secrets Eat more donuts.")
-	tester.execute("add task secrets Destroy all humans.")
-
-	log.Println("(deadline inclusion)")
-	tester.execute("deadline 1 1595352997")
-	tester.execute("deadline 2 1595352922")
-
-	log.Println("(show tasks)")
-	tester.execute("show")
-	tester.readLines([]string{
-		"secrets",
-		"    [ ] 1: (1595352997) Eat more donuts.",
-		"    [ ] 2: (1595352922) Destroy all humans.",
-		"",
-	})
-
-	log.Println("(quit)")
-	tester.execute("quit")
-
-	// make sure main program has quit
-	inPW.Close()
-	wg.Wait()
-
-	var err error
-	select {
-	case err = <-errorsChan:
-		log.Printf("program failed, %s", err)
-	case <-shutdownChan:
-		log.Println("finished")
-	}
-
-	if err != nil {
-		t.Fail()
-	}
-}
-
-func TestRun(t *testing.T) {
-	// setup input/output
-	log.SetOutput(io.Discard)
-	inPR, inPW := io.Pipe()
-	defer inPR.Close()
-	outPR, outPW := io.Pipe()
-	defer outPR.Close()
-	tester := &scenarioTester{
-		T:          t,
-		inWriter:   inPW,
-		outReader:  outPR,
-		outScanner: bufio.NewScanner(outPR),
-	}
-
-	// run main program
-	var wg sync.WaitGroup
-	shutdownChan := make(chan bool)
-	errorsChan := make(chan error)
-	initTaskListAndRun(wg, inPR, outPW, errorsChan, shutdownChan)
-
-	// run command-line scenario
-	log.Println("(show empty)")
-	tester.execute("show")
-
-	log.Println("(add project)")
-	tester.execute("add project secrets")
-	log.Println("(add tasks)")
-	tester.execute("add task secrets Eat more donuts.")
-	tester.execute("add task secrets Destroy all humans.")
-
-	log.Println("(show tasks)")
-	tester.execute("show")
-	tester.readLines([]string{
-		"secrets",
-		"    [ ] 1: Eat more donuts.",
-		"    [ ] 2: Destroy all humans.",
-		"",
-	})
-
-	log.Println("(add second project)")
-	tester.execute("add project training")
-	log.Println("(add more tasks)")
-	tester.execute("add task training Four Elements of Simple Design")
-	tester.execute("add task training SOLID")
-	tester.execute("add task training Coupling and Cohesion")
-	tester.execute("add task training Primitive Obsession")
-	tester.execute("add task training Outside-In TDD")
-	tester.execute("add task training Interaction-Driven Design")
-
-	log.Println("(check tasks)")
-	tester.execute("check 1")
-	tester.execute("check 3")
-	tester.execute("check 5")
-	tester.execute("check 6")
-
-	log.Println("(show completed tasks)")
-	tester.execute("show")
-	tester.readLines([]string{
-		"secrets",
-		"    [X] 1: Eat more donuts.",
-		"    [ ] 2: Destroy all humans.",
-		"",
-		"training",
-		"    [X] 3: Four Elements of Simple Design",
-		"    [ ] 4: SOLID",
-		"    [X] 5: Coupling and Cohesion",
-		"    [X] 6: Primitive Obsession",
-		"    [ ] 7: Outside-In TDD",
-		"    [ ] 8: Interaction-Driven Design",
-		"",
-	})
-
-	log.Println("(quit)")
-	tester.execute("quit")
-
-	// make sure main program has quit
-	inPW.Close()
-	wg.Wait()
-
-	var err error
-	select {
-	case err = <-errorsChan:
-		log.Printf("program failed, %s", err)
-	case <-shutdownChan:
-		log.Println("finished")
-	}
-
-	if err != nil {
-		t.Fail()
-	}
-
 }
 
 func initTaskListAndRun(wg sync.WaitGroup, inPR *io.PipeReader, outPW *io.PipeWriter, errorsChan chan error, shutdownChan chan bool) {
