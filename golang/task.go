0cce4c68a4f625091f14f8dfb73893d11065e3ac
Aldo <aldoft@hotmail.com>
2024-03-20 14:30:39 +0100

    move Go types to different files

--- golang/task.go
+++ golang/task.go
@@ -1,45 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
 	"time"
 )
-
-var timeFormat = time.DateOnly
-
-type deadline struct {
-	date time.Time
-}
-
-func NewDeadline(deadlineString string) (deadline, error) {
-	date, err := time.Parse(timeFormat, deadlineString)
-	if err != nil {
-		return deadline{}, err
-	}
-
-	return deadline{
-		date: date,
-	}, nil
-}
-
-func (d *deadline) String() string {
-	return fmt.Sprintf(" (%s)", d.date.Format(timeFormat))
-}
-
-func (d *deadline) IsEmpty() bool {
-	return d.date.IsZero()
-}
-
-type identifier int64
-
-func NewIdentifier(idString string) (identifier, error) {
-	id, err := strconv.ParseInt(idString, 10, 64)
-	if err != nil {
-		return -1, err
-	}
-	return identifier(id), nil
-}
 
 // Task describes an elementary task.
 type Task struct {
